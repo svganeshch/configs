863202571ca5fcd47801b7f1ca4cfd2218f17e46
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-01-25 14:12:21 +0000

    dashboard: Force reload from server

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
     $('body').on('click', '#btnArrowPie', function(){
         var version = $(this).val();
         $.post("setVersion.php", {"version": version});
-        location.reload();
+        location.reload(true);
     });
 
     $('body').on('click', '#btnArrowQ', function(){
         var version = $(this).val();
         $.post("setVersion.php", {"version": version});
-        location.reload();
+        location.reload(true);
     });
 });
